13473768c8cd4a2aeff1af4a57135cd3e135a702
darlincastellon <darlincastellon@gmail.com>
2015-03-13 23:48:28 -0600

    Mascota
    Clase mascota

---
+++ gestionPerrera/ConsoleApplication3/Mascota.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPerrera
+{
+   public class Mascota
+    {
+        private String strNombre;
+        private int intEdad;
+        private String strRaza;
+
+
+        public Mascota(String nombre, int edad, String raza)
+        {
+            this.strNombre = nombre;
+            this.strRaza = raza;
+            this.intEdad = edad;
+        }
+
+
+        public string Nombre
+        {
+            set
+            {
+                strNombre = value;
+            }
+            
+            get
+            {
+                return strNombre;
+            }
+           
+        }
+
+
+
+
+        public string Raza
+        {
+
+            set
+            {
+                strRaza = value;
+            }
+
+            get
+            {
+                return strRaza;
+            }
+
+        }
+
+
+        public int Edad
+        {
+            set
+            {
+                intEdad = value;
+            }
+            
+            get
+            {
+                return intEdad;
+            }
+
+            
+        }
+
+       
+
+		
+
+    }
+}
